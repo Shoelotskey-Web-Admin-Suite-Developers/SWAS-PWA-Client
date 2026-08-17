4516e8ad28616f90e14f643a319bb6f8f21c3826
Luigi Lagman <26.llagman@gmail.com>
2026-08-17 14:57:18 +0800

    update ui of close button in dialog

--- client/src/components/ui/dialog.tsx
+++ client/src/components/ui/dialog.tsx
@@ -42,8 +42,8 @@ const DialogContent = React.forwardRef<
       {...props}
     >
       {children}
-      <DialogPrimitive.Close className="absolute right-4 top-4 rounded-sm p-1 transition-colors hover:bg-transparent focus:outline-none disabled:pointer-events-none data-[state=open]:bg-accent data-[state=open]:text-muted-foreground">
-        <X className="h-4 w-4 text-gray-500 hover:text-[#CE1616] focus:text-[#CE1616] active:text-[#CE1616]" />
+      <DialogPrimitive.Close className="absolute right-4 top-4 rounded-sm p-1 transition-colors hover:bg-transparent focus:outline-none disabled:pointer-events-none data-[state=open]:bg-accent data-[state=open]:text-muted-foreground [&>svg]:!bg-transparent [&>svg]:!shadow-none">
+        <X className="h-4 w-4 text-gray-500 hover:text-[#CE1616] focus:text-[#CE1616] active:text-[#CE1616] !bg-transparent" />
         <span className="sr-only">Close</span>
       </DialogPrimitive.Close>
     </DialogPrimitive.Content>
